6d21490c22b7261d59b95bdb29be1e87884c61f1
Stefan Nachtrab <steve@steve4u.de>
2019-02-21 14:07:29 +0100

    optimized copy

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f "/opt/pyload/pyload-config/pyload.conf" ]
 then
-    cp -rf /opt/pyload/pyload-initial-config /opt/pyload/pyload-config/
+    cp -afrv /opt/pyload/pyload-initial-config/* /opt/pyload/pyload-config/
 fi
 
 /opt/pyload/pyLoadCore.py --configdir=/opt/pyload/pyload-config/
