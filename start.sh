da2faeb2328269c5b8a55bbde42c085922733c68
Stefan Nachtrab <steve@steve4u.de>
2019-02-21 14:24:00 +0100

    check pyload pid file and delete

--- start.sh
+++ start.sh
@@ -5,4 +5,9 @@ then
     cp -afrv /opt/pyload/pyload-initial-config/* /opt/pyload/pyload-config/
 fi
 
+if [ -f "/opt/pyload/pyload-config/pyload.pid" ]
+then
+    rm /opt/pyload/pyload-config/pyload.pid
+fi
+
 /opt/pyload/pyLoadCore.py --configdir=/opt/pyload/pyload-config/
