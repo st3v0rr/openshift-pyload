1d6f674b6fc3785d8fbf469126c54e759c384bf7
Stefan Nachtrab <steve@steve4u.de>
2019-02-21 14:02:08 +0100

    copy overwrite

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 if [ ! -f "/opt/pyload/pyload-config/pyload.conf" ]
 then
     mkdir /opt/pyload/pyload-config/
-    cp /opt/pyload/pyload-initial-config /opt/pyload/pyload-config/
+    cp -rf /opt/pyload/pyload-initial-config /opt/pyload/pyload-config/
 fi
 
 /opt/pyload/pyLoadCore.py --configdir=/opt/pyload/pyload-config/
